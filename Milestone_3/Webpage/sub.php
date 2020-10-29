3bd40c41169cf1cf97e65763de486816eee97180
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-10-28 21:07:49 -0400

    Update sub.php

--- Milestone_3/Webpage/sub.php
+++ Milestone_3/Webpage/sub.php
@@ -8,6 +8,8 @@ $conn = new mysqli_connect($server, $user, $pwd, $db);
 if($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
 }
+else print "<br>Connection OK!";
+
 $sql_select = "SELECT * FROM Subscribe";
 if($res = $conn->query($sql_select)) {                                                                                          while($row = $res->fetch_assoc()) {
         	echo "User: " . $row["User_name"] . "    Channel ID: " . $row["Channel_id"] . "  Start date: " . $row["Start_date"] . "<br>";
