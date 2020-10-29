7c9bcd22ec0b1d64ed95bc1b4d14c0066000b83c
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-10-29 12:13:35 -0400

    Create phpUpdateFormScript.php

---
+++ Milestone_3/Webpage/phpUpdateFormScript.php
@@ -0,0 +1,29 @@
+<?php
+
+$name = $_POST["name"];
+$First_name = $_POST["First_name"];
+$Last_name = $_POST["Last_name"];
+$Password = $_POST["Password"];
+
+$server = "localhost";
+$user = "kguo";
+$pwd = "17417174";
+$db = "xmin2_1";
+
+$conn = new mysqli($servername, $username, $password, $db);
+
+if ($conn->connect_error){
+	die("Connection failed: ". $conn->connect_error);
+}
+
+$sql = "update users set First_name='$First_name', Last_name='$Last_name', Password='$Password' where name='$name'";
+
+if ($conn->query($sql) === TRUE) {
+	echo "Records updated: ".$name."-".$First_name."-".$Last_name."-".$Password;
+} else {
+	echo "Error: ".$sql."<br>".$conn->error;
+}
+
+$conn->close();
+
+?>
