a960008159a19c59034a7958282a156cf00b7ce8
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-10-28 21:00:31 -0400

    Update like.php

--- Milestone_3/Webpage/like.php
+++ Milestone_3/Webpage/like.php
@@ -1,24 +1,23 @@
-<!-- <?php
+<?php
 $server = "localhost";
 $user = "kguo";
-$pwd = "17417174"
-$db = "kguo_1"
+$pwd = "17417174";
+$db = "kguo_1";
 
 $conn = new mysqli_connect($server, $user, $pwd, $db);                                                                  
 if($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-}                                                                                                                       
+}
+else print "<br>Connection OK!";
 $sql_select = "SELECT * FROM likes";
 if($res = $conn->query($sql_select)) {                                                                                          while($row = $res->fetch_assoc()) {
         	echo "User: " . $row["User_name"] . "    Video ID: " . $row["Video_id"] . "  Comment: " . $row["Comment"] . "	Repeated views: " . $row["Repeated_views"] . "<br>";
 	}
 }
-?> -->
-
-
-
-<?php
-echo "My first PHP script!";
 ?>
+
+
+
+
 
 
