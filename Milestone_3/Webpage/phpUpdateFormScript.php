48ae03ad9bb75ca28e2a7c38df202cf6d8c8a211
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-10-29 12:15:35 -0400

    Update phpUpdateFormScript.php

--- Milestone_3/Webpage/phpUpdateFormScript.php
+++ Milestone_3/Webpage/phpUpdateFormScript.php
@@ -3,7 +3,7 @@
 $name = $_POST["name"];
 $First_name = $_POST["First_name"];
 $Last_name = $_POST["Last_name"];
-$Password = $_POST["Password"];
+$Password = $_POST["password"];
 
 $server = "localhost";
 $user = "kguo";
@@ -19,7 +19,7 @@ if ($conn->connect_error){
 $sql = "update users set First_name='$First_name', Last_name='$Last_name', Password='$Password' where name='$name'";
 
 if ($conn->query($sql) === TRUE) {
-	echo "Records updated: ".$name."-".$First_name."-".$Last_name."-".$Password;
+	echo "Records updated: ".$name."-".$First_name."-".$Last_name."-".$password;
 } else {
 	echo "Error: ".$sql."<br>".$conn->error;
 }
