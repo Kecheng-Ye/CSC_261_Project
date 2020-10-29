d18cfc06284534314f989094a33ff052b46a70af
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-10-29 12:17:08 -0400

    Update phpUpdateFormScript.php

--- Milestone_3/Webpage/phpUpdateFormScript.php
+++ Milestone_3/Webpage/phpUpdateFormScript.php
@@ -10,7 +10,7 @@ $user = "kguo";
 $pwd = "17417174";
 $db = "xmin2_1";
 
-$conn = new mysqli($servername, $username, $password, $db);
+$conn = new mysqli($server, $user, $pwd, $db);
 
 if ($conn->connect_error){
 	die("Connection failed: ". $conn->connect_error);
