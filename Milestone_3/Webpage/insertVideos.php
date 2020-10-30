822bd346d60fdfb78cae0394266280c1374b0f34
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-10-29 23:22:04 -0400

    Create insertVideos.php

---
+++ Milestone_3/Webpage/insertVideos.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+
+$video_id = $_POST['video_id'];
+$title = $_POST['title'];
+$channel_id = $_POST['channel_id'];
+$publish_date = $_POST['publish_date'];
+$views = $_POST['views'];
+
+echo $video_id."<br>".$title."<br>".$channel_id."<br>".$publish_date."<br>".$views."<br>";
+?>
+
+
+<form action="phpInsertVideoForm.php" method="post">
+video_id: <input type="text" name="video_id"><br>
+channel_id: <input type="text" name="channel_id"><br>
+publish_date: <input type="text" name="publish_date"><br>
+views: <input type="text" name="views"><br>
+
+<input type ="submit">
+</form>
+
+
+
+</body>
+</html>
+
+
+
+
