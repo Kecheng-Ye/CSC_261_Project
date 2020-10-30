f246eb2b8e09b906e49368df2fb16a42654cbc13
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-10-29 23:11:52 -0400

    Create phpInsertVideoForm.php

---
+++ Milestone_3/Webpage/phpInsertVideoForm.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+$video_id = $_POST['video_id'];
+$title = $_POST['title'];
+$channel_id = $_POST['channel_id'];
+$publish_date = $_POST['publish_date'];
+$views = $_POST['views'];
+
+$server = "localhost";
+$user = "kguo";
+$pwd = "17417174";
+$db = "kguo_1";
+
+$conn = new mysqli($server, $user, $pwd, $db);
+
+if ($conn->connect_error){
+	die("Connection failed: ". $conn->connect_error);
+}
+
+$sql = "insert into videos(video_id,title,channel_id,publish_date,views) values($video_id,$title,$channel_id,$publish_date,$views)";
+
+if ($conn->query($sql) === TRUE) {
+	echo "ADDED: ".$video_id.", ".$title.", ".$channel_id", ".$publish_date", ".$views";
+} else {
+	echo "Error: ".$sql."<br>".$conn->error;
+}
+
+$conn->close();
+
+?>
+
+</body>
+</html>
