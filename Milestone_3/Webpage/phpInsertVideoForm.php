6ba68b226cfef595dc0059fe9a852c87ac81ca59
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-10-30 00:03:48 -0400

    Update phpInsertVideoForm.php

--- Milestone_3/Webpage/phpInsertVideoForm.php
+++ Milestone_3/Webpage/phpInsertVideoForm.php
@@ -2,7 +2,7 @@
 
 $id= $_POST['id'];
 $Title = $_POST['Title'];
-$Channel_ID = $_POST['Channel id'];
+$Channel_ID = $_POST['Channel_id'];
 $Publish_date = $_POST['Publish_date'];
 $Views = $_POST['Views'];
 
@@ -17,7 +17,7 @@ if ($conn->connect_error){
 	die("Connection failed: ". $conn->connect_error);
 }
 
-$sql = "insert into videos(id,Title,Channel_ID,Publish_date,Views) values('$id','$Title','$Channel_ID','$Publish_date','$Views')";
+$sql = "insert into videos(video_id,title,channel_id,publish_date,views) values('$id','$Title','$Channel_ID','$Publish_date','$Views')";
 
 if ($conn->query($sql) === TRUE) {
 	echo "ADDED: ".$id.", ".$Title.", ".$Channel_ID", ".$Publish_date", ".$Views;
