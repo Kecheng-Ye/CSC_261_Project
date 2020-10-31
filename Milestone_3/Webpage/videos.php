089f46e3edf9aa949829511ecfdba6af096e9dc9
Guo Kehao <kguo@betaweb.csug.rochester.edu>
2020-10-30 22:10:30 -0400

    fixed videos.php typo

--- Milestone_3/Webpage/videos.php
+++ Milestone_3/Webpage/videos.php
@@ -18,7 +18,7 @@ if($conn->connect_error) {
 $sql_select = "SELECT * FROM Video";
 if($res = $conn->query($sql_select)) {
 	while($row = $res->fetch_assoc()) {
-		echo "Video ID: " . $row["id"] . "    Title: " . $row["Title"] . "  Channel_ID: " . $row["Channel id"] . "    Publish date: " . $row["Publish_date"] . "        Views: " . $row["Views"] . "<br>";
+		echo "Video ID: " . $row["id"] . "    Title: " . $row["Title"] . "  Channel_ID: " . $row["Channel_id"] . "    Publish date: " . $row["Publish_date"] . "        Views: " . $row["Views"] . "<br>";
 	}
 }
 
