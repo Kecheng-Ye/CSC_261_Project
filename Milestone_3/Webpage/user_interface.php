1a3709a446b09fb799452f849b1b998abd8ea828
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-11-01 23:11:54 -0500

    Update user_interface.php

--- Milestone_3/Webpage/user_interface.php
+++ Milestone_3/Webpage/user_interface.php
@@ -103,28 +103,63 @@ h2 {
      Video information
     Args:  usr_name(string): the user name used to do query
            table(string): the table we are doing query                     */
-  function like_info($usr_name, $table){
+//   function like_info($usr_name, $table){
+//     global $name, $conn, $name_KV, $is_login;
+
+//     if($conn->connect_error) {
+//       die("Connection failed: " . $conn->connect_error);
+//     } 
+//     if($is_login) {
+// //       $sql_select = "SELECT * FROM $table WHERE $name_KV[$table] = \"$usr_name\"";
+//       $sql_select = "SELECT Video_id FROM likes WHERE name = \"$usr_name\"";
+//       if($res = $conn->query($sql_select)) {
+//         # one user may like a lot of videos
+//         while($row = $res->fetch_assoc()) {
+//           $count = 1;
+//           echo $count. ".\t";
+//           while($element = current($row)) {
+//             if(key($row) == $name_KV[$table]){
+//             }else if (key($row) == "Video_id"){ 
+//               # since print out the video id will not be user friendly
+//               # the function will do a second query to search for the Video title of that video id 
+//               # and print out it
+//               $id = key($row);
+// //               $video_query = "SELECT Title FROM Video WHERE id = " . "\"" . $row[$id] . "\"";
+// 	      $video_query = "SELECT * FROM Video WHERE id = " . "\"" . $row[$id] . "\"";
+//               $query = $conn->query($video_query);
+//               $result = $query->fetch_assoc();
+//               echo key($result).":\t" . $result[key($result)] . "<br></br>";
+//             }else{
+//               echo key($row).":\t" . $row[key($row)] . "<br></br>";
+//             }
+            
+//             next($row);
+//           }
+//           $count += 1;
+//         }
+//       }
+//     }
+//   }
+	
+	
+function like_info($usr_name, $table){
     global $name, $conn, $name_KV, $is_login;
 
     if($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
     } 
     if($is_login) {
-//       $sql_select = "SELECT * FROM $table WHERE $name_KV[$table] = \"$usr_name\"";
       $sql_select = "SELECT Video_id FROM likes WHERE name = \"$usr_name\"";
       if($res = $conn->query($sql_select)) {
         # one user may like a lot of videos
         while($row = $res->fetch_assoc()) {
-          $count = 1;
-          echo $count. ".\t";
           while($element = current($row)) {
-            if(key($row) == $name_KV[$table]){
-            }else if (key($row) == "Video_id"){ 
+            if (key($row) == "Video_id"){ 
               # since print out the video id will not be user friendly
               # the function will do a second query to search for the Video title of that video id 
               # and print out it
               $id = key($row);
-//               $video_query = "SELECT Title FROM Video WHERE id = " . "\"" . $row[$id] . "\"";
+
 	      $video_query = "SELECT * FROM Video WHERE id = " . "\"" . $row[$id] . "\"";
               $query = $conn->query($video_query);
               $result = $query->fetch_assoc();
@@ -135,11 +170,11 @@ h2 {
             
             next($row);
           }
-          $count += 1;
         }
       }
     }
   }
+
 
 
   /* This function will do a trivial query of subscribe table and print all User subscribe 
