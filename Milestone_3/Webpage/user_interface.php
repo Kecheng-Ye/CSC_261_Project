52da4788b828eefb846c65bf615395c355007251
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-11-02 00:40:54 -0500

    Update user_interface.php

--- Milestone_3/Webpage/user_interface.php
+++ Milestone_3/Webpage/user_interface.php
@@ -189,7 +189,7 @@ h2 {
         while($element = current($row)) {
     if(key($row) != $name_KV[$table]){
 	  $id = $row[key($row)];
-          $video_query = "SELECT Title FROM Video WHERE id = \"$id\"";
+          $video_query = "SELECT * FROM Video WHERE id = \"$id\"";
           $query = $conn->query($video_query);
           $result = $query->fetch_assoc();
          echo key($result).":\t" . $result[key($result)] . "<br></br>";
