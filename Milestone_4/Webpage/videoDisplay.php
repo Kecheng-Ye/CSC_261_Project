215a7606f2194ed4807f0910dec4de131a08754d
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-11-28 11:02:58 -0500

    Update videoDisplay.php

--- Milestone_4/Webpage/videoDisplay.php
+++ Milestone_4/Webpage/videoDisplay.php
@@ -2,9 +2,8 @@
 <html>
 <body>
 
-<!-- <iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1">
-</iframe> -->
-    <a href= "www.google.com">     <input type="button"/>   </a>
+<iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1">
+</iframe>
 
 </body>
 </html>
