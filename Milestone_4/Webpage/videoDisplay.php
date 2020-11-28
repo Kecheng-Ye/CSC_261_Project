351ca7c5b555ad97b2855d70d10f2788b1ec32a0
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-11-28 13:05:09 -0500

    Update videoDisplay.php

--- Milestone_4/Webpage/videoDisplay.php
+++ Milestone_4/Webpage/videoDisplay.php
@@ -20,6 +20,8 @@ if($conn->connect_error){
  
 echo $name;
 echo $videoId;
+$url = "https://www.youtube.com/embed/".$videoId;
+echo' <iframe width="420" height="315" src=' .$url. '> </iframe>';
 
 // <iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1">
 // </iframe>
