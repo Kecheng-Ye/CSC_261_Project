84f1ba3dfca411c77126d3388b20426412497f78
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-11-28 01:17:28 -0500

    Update user_interface.php

--- Milestone_4/Webpage/user_interface.php
+++ Milestone_4/Webpage/user_interface.php
@@ -129,7 +129,7 @@ function like_info($usr_name, $table){
 					echo'<form method="POST" action="videoDisplay.php">
      <input type="button" name="watch"/>';
 					
-					echo'   <a href="https://www.youtube.com/embed/"> + <href = "<?php echo $row[$id]?>"> + <href = "?autoplay=1&mute=1">>
+					echo'   <a href="https://www.youtube.com/embed/"> <href = "<?php echo $row[$id]?>">  <href = "?autoplay=1&mute=1">>
      <input type="button"/>
    </a>';
 					 
