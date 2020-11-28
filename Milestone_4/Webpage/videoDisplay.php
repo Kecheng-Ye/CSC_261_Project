507bc95c128c95fb9a6c9bf5306a3fbad6ceece2
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-11-28 12:42:33 -0500

    Update videoDisplay.php

--- Milestone_4/Webpage/videoDisplay.php
+++ Milestone_4/Webpage/videoDisplay.php
@@ -1,9 +1,32 @@
 <!DOCTYPE html>
 <html>
 <body>
+  
+ <?php
+$videoId = $_POST["row[$id]"];
+$usr_name = $_POST["name"];
 
-<iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1">
-</iframe>
+
+$server = "localhost";
+$user = "kguo";
+$pwd = "17417174";
+$db = "kguo_1";
+
+$conn = new mysqli($server, $user, $pwd, $db);
+
+if($conn->connect_error){
+	die("Connection failed ".$conn->connect_error);
+}
+ 
+echo $usr_name;
+echo $videoId;
+
+// <iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1">
+// </iframe>
+  
+$conn->close();
+
+?>
 
 </body>
 </html>
