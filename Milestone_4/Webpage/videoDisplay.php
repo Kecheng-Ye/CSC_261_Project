125839c92166a1b84319ccfd45ef0c36de4c7e80
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-11-28 13:50:39 -0500

    Update videoDisplay.php

--- Milestone_4/Webpage/videoDisplay.php
+++ Milestone_4/Webpage/videoDisplay.php
@@ -32,9 +32,13 @@ if(array_key_exists('goback', $_POST)) {
         } 
 function goback() { 
 //             echo "This is Button1 that is selected"; 
-	   echo '<form action="user_interface.php"  method="post">
+   echo ' <form action="user_interface.php" id="goto_Demo" method="post">
         <input type ="hidden", name="name", value= '.$name.'>
-    </form>';
+    </form>
+
+    <script type="text/javascript">
+        document.getElementById("goto_Demo").submit();
+    </script>';
         } 
 // if(isset($_POST['return'])) { 
 // //    echo '<form action="user_interface.php"  method="post">
