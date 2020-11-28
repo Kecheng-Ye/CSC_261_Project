1c7f4f06e0481c28fc7703fc164617a02583f377
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-11-28 10:49:10 -0500

    Update user_interface.php

--- Milestone_4/Webpage/user_interface.php
+++ Milestone_4/Webpage/user_interface.php
@@ -129,7 +129,10 @@ function like_info($usr_name, $table){
 					
 					$url = "https://www.youtube.com/embed/".$row[$id];
 					echo $url;
-					echo '<a src=<?php echo $url?>> <input type="button"/>   </a>'
+					echo '<form action="https://google.com">
+    <input type="submit" value="Go to Google" />
+</form>'
+// 					echo '<a src=<?php echo $url?>> <input type="button"/>   </a>'
 // 					echo'   <a href= "www.google.com">     <input type="button"/>   </a>';
 					 
 // 					echo $row[$id];
