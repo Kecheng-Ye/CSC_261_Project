00fb1f4032b56370f009e91b21be7989734c5517
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-11-28 22:32:10 -0500

    Update videoDisplay.php

--- Milestone_4/Webpage/videoDisplay.php
+++ Milestone_4/Webpage/videoDisplay.php
@@ -35,12 +35,12 @@ function goback() {
 	global $name;
 	echo $name;
 //             echo "This is Button1 that is selected"; 
-     echo '<form action="user_interface.php" id="goto_Demo" method="post">
-        <input type ="hidden", name="goback", value='.\"$name\".'>
+     echo '<form action=\"user_interface.php\" id=\"goto_Demo\" method=\"post\">
+        <input type =\"hidden\", name=\"goback\", value=\"$name\">
     </form>
 
-    <script type="text/javascript">
-        document.getElementById("goto_Demo").submit();
+    <script type=\"text/javascript\">
+        document.getElementById(\"goto_Demo\").submit();
     </script>';
         } 
 // if(isset($_POST['return'])) { 
