c0c33bada7df10f08966df6e3e3c11853d9f076f
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2020-12-01 23:13:38 -0500

    Update channels.php

--- Milestone_4/Webpage/channels.php
+++ Milestone_4/Webpage/channels.php
@@ -9,8 +9,37 @@
   <script>
 	includeHTML();
   </script>
+	
+	
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+	
+	
+	
 </head>
-<body>
+	<body>
+	<table>
+<tr>
+    <th>Channel ID</th>
+    <th>Title</th>
+    <th>Release Date</th>
+  </tr>
+
 	<div include-html="./styles/navibar_main.php"></div>
 <?php
 include "utils.php";
@@ -24,18 +53,25 @@ if($conn->connect_error) {
 $sql_select = "SELECT * FROM Channel";
 if($res = $conn->query($sql_select)) {
 	while($row = $res->fetch_assoc()) {
-		echo "Channel_ID: " . $row["id"] . "	Name: " . $row["Title"] . "	Joined since: " . $row["Release_date"] . "<br>";
+// 		echo "Channel_ID: " . $row["id"] . "	Name: " . $row["Title"] . "	Joined since: " . $row["Release_date"] . "<br>";
+		 echo " <tr>";
+   echo " <td>" .$row["id"]."</td>";
+   echo "<td>" . $row["Title"] ."</td>";
+   echo " <td>".$row["Release_date"]."</td>";
+ echo " </tr>";
 	}
 }
 
 mysqli_close($conn);
 ?>
+		
+
 	
 <div include-html="./styles/footer.html"></div>
 
  <script>
   includeHTML();
 </script>
-
+</table>
 </body>
 </html>
